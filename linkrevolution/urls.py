7c16f3a6cbe910136968a452ad124e9ce22fe857
tsukasarenato <tsukasa.renato@gmail.com>
2022-07-31 20:08:51 -0300

    serve media files

--- linkrevolution/urls.py
+++ linkrevolution/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('websites.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
 
 if settings.DEBUG:
     urlpatterns = [
