19305cd882cd1bfff02f76639b3487a1602c0f02
tsukasarenato <tsukasa.renato@gmail.com>
2022-08-25 23:34:20 -0300

    update django lib, api token authenticator

--- linkrevolution/settings.py
+++ linkrevolution/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -113,7 +114,10 @@ INTERNAL_IPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
